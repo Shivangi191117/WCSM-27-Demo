271098fb71b7a4fe6c644b9794d9bf7b84981b9c
shivangi <shivbans1911@gmail.com>
2023-04-14 10:59:15 +0530

    Branch is commited.

---
+++ src/test/java/SampleGithub/DemoTest.java
@@ -0,0 +1,18 @@
+package SampleGithub;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+
+	@Test
+	
+	public void printStatement() {
+		System.out.println("hellow this is new class");
+		System.out.println("hii u are seeing the changes");
+		
+		
+	}
+
+}
+
